1babbfd8f7bcb1fa9a80f61639d2409acb91668d
Barbora Lomenova <barbora.lomenova@gmail.com>
2025-01-23 11:11:16 +0100

    prvy_js

---
+++ script.js
@@ -0,0 +1,9 @@
+//document.body.innerHTML += 'Ahoj ja som javascript';
+
+//document.body.innerHTML += 'Měsíční příjem je ' + ( 7 * 320 * 21) + 'Kc.';
+//document.body.innerHTML += 'Měsíční příjem je ' + ( 7 * 320 * 21) + 'Kc.';
+//document.body.innerHTML += 'Zaplatim dan'+ Math.floor((0.4*0.15))
+//document.body.innerHTML += 223%60;
+
+document.body.innerHTML += 'barbora' + '.' + 'lomenova'+ '@gmail.com'
+
