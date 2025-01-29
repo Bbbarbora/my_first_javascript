37052bfa8de607c5f722106034d05449caebbb7a
Barbora Lomenova <barbora.lomenova@gmail.com>
2025-01-29 16:08:56 +0100

    ulohy kocka

--- script.js
+++ script.js
@@ -5,10 +5,47 @@
 //document.body.innerHTML += 'Zaplatim dan'+ Math.floor((0.4*0.15))
 //document.body.innerHTML += 223%60;
 
-document.body.innerHTML += 'barbora' + '.' + 'lomenova'+ '@gmail.com';
+// document.body.innerHTML += 'barbora' + '.' + 'lomenova'+ '@gmail.com';
 
-document.body.innerHTML += "<br>";
+// document.body.innerHTML += "<br>";
 
-document.body.innerHTML += Math.random() *10;
-document.body.innerHTML += "<br>"
-document.body.innerHTML += Math.ceil((Math.random() *10))
+// document.body.innerHTML += Math.random() *10;
+// document.body.innerHTML += "<br>"
+// document.body.innerHTML += Math.ceil((Math.random() *10))+ "<br>"
+
+// const wageInEur= 20
+// const wageInCzk= Math.round(wageInEur* 24.55)
+// document.body.innerHTML += "<h1>Mzda v korunách: " + wageInCzk +  "KC</h1>" + "<br>";
+
+// const count = 5;
+// let name= "Alena";
+
+// let name;
+// name ='Pavel';
+// document.body.innerHTML += name;
+
+// const sazba = 500;
+// const pocetHodin = 147;
+// const vyplata = sazba * pocetHodin;
+// document.body.innerHTML += vyplata + "<br>";
+
+// let pocet = 0;
+// pocet = pocet +1;
+// document.body.innerHTML += pocet;
+
+let email = "alena.novakova";
+email+= "@gmail.com";
+document.body.innerHTML += email + "<br>",
+
+document.body.innerHTML+= Math.random() +"<br>";
+
+
+const wageEur= 20;
+const wageInCzk = Math.round(wageEur * 24.55);
+document.body.innerHTML += "<h1>Mzda v korunách:" + wageInCzk + "KC</h1>";
+
+const start= 15;
+const delka = 10;
+const konec= start + delka;
+document.body.innerHTML += "<h1>Zavod skoncil v " + konec % 24 + " hodin<h1>";
+
